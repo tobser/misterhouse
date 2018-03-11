ee2532b036d7bcac563d09ff6e2b853a124eb3a9
Tobias Sachs <diespambox@gmx.net>
2018-03-11 12:36:54 +0100

    ia7: fix initialization of zoneminder module

--- web/ia7/include/javascript.js
+++ web/ia7/include/javascript.js
@@ -1288,9 +1288,13 @@ var updateStaticPage = function(link,time) {
 								}
 							});
 						}																
-					}
-					generateTooltips();
-			
+                    }
+                    $(".btn-state-cmd").mayTriggerLongClicks().on( 'longClick', function() {		        
+                        var entity = $(this).attr("entity");
+                        create_state_modal(entity);
+                    });						
+                    generateTooltips();
+
 				});
 			}
 			if (jqXHR.status == 200 || jqXHR.status == 204) {
@@ -4209,6 +4213,7 @@ var zoneminder = function()
 {
     loadModule('zoneminder', function(){
         zm.init();
+        var config = json_store.ia7_config.zoneminder;
         for (i = 0; i < config.length; ++i) {
             zm.connect_server(config[i]);
         }
